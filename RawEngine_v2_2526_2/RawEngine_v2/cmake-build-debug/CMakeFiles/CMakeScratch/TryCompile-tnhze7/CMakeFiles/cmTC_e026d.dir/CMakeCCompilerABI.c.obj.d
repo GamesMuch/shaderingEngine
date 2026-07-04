00d5452b1ed7989556ffb1a5ad781c293704f723
GamesMuch <everydaycrevan@gmail.com>
2026-07-04 23:33:25 +0200

    A push?!

---
+++ RawEngine_v2_2526_2/RawEngine_v2/cmake-build-debug/CMakeFiles/CMakeScratch/TryCompile-tnhze7/CMakeFiles/cmTC_e026d.dir/CMakeCCompilerABI.c.obj.d
@@ -0,0 +1,1 @@
+C:/Program Files/JetBrains/CLion 2025.2.3/bin/cmake/win/x64/share/cmake-4.0/Modules/CMakeCompilerABI.h
---
+++ RawEngine_v2_2526_2/RawEngine_v2/cmake-build-debug/CMakeFiles/CMakeScratch/TryCompile-tnhze7/CMakeFiles/cmTC_e026d.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmTC_e026d.dir/CMakeCCompilerABI.c.obj"
+  "CMakeFiles/cmTC_e026d.dir/CMakeCCompilerABI.c.obj.d"
+  "cmTC_e026d.exe"
+  "cmTC_e026d.exe.manifest"
+  "cmTC_e026d.lib"
+  "cmTC_e026d.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cmTC_e026d.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
